1d21dea991bbb869afc60475476b583c965a14b5
cgrunwald <cyprian.grunwald@altkomsoftware.pl>
2022-08-18 13:50:48 +0200

    Add storage of request info for particular logins

--- users/src/main/java/pl/grunwald/users/UserRequestInfoRepository.java
+++ users/src/main/java/pl/grunwald/users/UserRequestInfoRepository.java
@@ -1,11 +1,14 @@
 package pl.grunwald.users;
 
 import java.util.Optional;
+import javax.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 interface UserRequestInfoRepository extends CrudRepository<UserRequestInfo, Integer> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<UserRequestInfo> findByLogin(String login);
 }
