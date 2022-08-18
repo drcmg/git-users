1457d8c73d9be71dd4d69bffddd6ede4d01c5fc8
cgrunwald <cyprian.grunwald@altkomsoftware.pl>
2022-08-18 12:23:20 +0200

    Add mapping config to RestTemplate to correctly mapping snake_case properties from api

--- users/src/main/java/pl/grunwald/users/integration/configuration/UserInfoConfiguration.java
+++ users/src/main/java/pl/grunwald/users/integration/configuration/UserInfoConfiguration.java
@@ -1,14 +1,25 @@
 package pl.grunwald.users.integration.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class UserInfoConfiguration {
 
+    private final ObjectMapper objectMapper;
+
+    public UserInfoConfiguration(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        return new RestTemplateBuilder()
+                .messageConverters(new MappingJackson2HttpMessageConverter(objectMapper))
+                .build();
     }
 }
